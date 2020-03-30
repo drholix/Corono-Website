5118145d0727a20d0b9ff19e3749fd01f9da6570
drholix <drholix@gmail.com>
2020-03-30 16:08:49 +0700

    First Commit

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+document.getElementById("speaker_on").addEventListener("click", showSpeaker);
+document.getElementById("speaker_off").addEventListener("click", hideSpeaker);
+
+function showSpeaker() {
+  document.getElementById("speaker_on").classList.add("hide");
+  document.getElementById("speaker_off").classList.add("show");
+  document.getElementById("speaker_on").classList.remove("show");
+  document.getElementById("corona").muted = true;
+}
+
+function hideSpeaker() {
+  document.getElementById("speaker_on").classList.add("show");
+  document.getElementById("speaker_off").classList.remove("hide");
+  document.getElementById("speaker_off").classList.remove("show");
+  document.getElementById("corona").muted = false;
+}
