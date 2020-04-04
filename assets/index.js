b25688d7d491547de2d43f2d99b952f93aafff7d
drholix <drholix@gmail.com>
2020-04-04 14:39:07 +0700

    90% responsif web

--- assets/index.js
+++ assets/index.js
@@ -17,8 +17,44 @@ function hideSpeaker() {
 
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
+  document.getElementById("span-nav").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
+  document.getElementById("span-nav").style.display = "block";
 }
+
+if (screen.width <= 1100) {
+  openNav();
+  closeNav();
+}
+
+// function openNav() {
+//   let query = window.matchMedia("(max-width: 1024px)");
+
+//   if (query.matches) {
+
+//   }
+// }
+
+// var x = window.matchMedia("(min-width: 1024px)");
+// openNav(x);
+// x.addListener(closeNav);
+
+// console.log(x);
+
+// document.getElementById("span-nav").addEventListener("click", showNav);
+// document.getElementById("span-nav-hide").addEventListener("click", hideNav);
+
+// function showSpeaker() {
+//   document.getElementById("#span-nav").classList.add("span-nav-hide");
+//   document.getElementById("speaker_off").classList.add("show");
+
+// document.getElementById("span-nav").onclick = function() {
+//   document.getElementById("span-nav").style.display = "none";
+// };
+
+// function openNav() {
+//   document.getElementById("span-nav").style.display = "none";
+// }
